b53aa3f326d2131f170f29795eaf3fd620ff4ea4
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2021-03-07 22:50:03 -0300

    Listing menu items

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
+import json
+from pathlib import Path
 from init import app
 from flask import jsonify
+
+
+@app.route("/menu", methods=['GET'])
+def menu_index():
+    menu_path = Path("static", "menu.json")
+    with open(menu_path) as fp:
+        menu = json.loads(fp.read())
+        return jsonify(menu)
 
 
 @app.route("/", methods=['GET'])
